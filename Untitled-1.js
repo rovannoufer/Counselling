550dbfc8547729f0874e2b23da32bfbbecd92e99
Noufer Mohamed A <90058162+rovannoufer@users.noreply.github.com>
2023-05-31 07:23:31 +0530

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -52,3 +52,116 @@ mongo.connect(url,(err,db)=>{
 app.listen(3000,()=>{
     console.log("Server running")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
+const app = express();
+const url = "mongodb://127.0.0.1:27017/model";
+mongo.connect(url, (err, db) => {
+  if (err) throw err;
+  var dbc = db.db("semesteriwp");
+  app.get("/insert", (req, res) => {
+    let data =[{
+      model_name:"apple",
+      make:"USA",
+      color:"deep purple",
+      price:"150000"
+    },
+    {
+      model_name:"samsung",
+      make:"china",
+      color:"black",
+      price:"80000"
+    },
+    {
+      model_name:"vivo",
+      make:"china",
+      color:"white",
+      price:"25000"
+    },
+    {
+      model_name:"realme",
+      make:"china",
+      color:"red",
+      price:"30000"
+    },
+    {
+      model_name:"oppo",
+      make:"korea",
+      color:"blue",
+      price:"30000"
+    }
+    ];
+    dbc.collection("bike_datas").insertMany(data, (err, res) => {
+      if (err) throw err;
+      console.log("Data inserted");
+    });
+  });
+
+  app.get("/getdata", (req, res) => {
+    let data = { model_name:"oppo" };
+    dbc.collection("bike_datas").findOne(data, (err, res) => {
+      if (err) throw err;
+      console.log(res);
+    });
+  });
+
+  app.get("/read", (req, res) => {
+    // let data = { model_name: "BMW" };
+    dbc.collection("bike_datas").find({}).toArray(function(err, result)
+     {
+        if (err) throw err;
+        console.log(result);
+    });
+  });
+
+  // app.get("/update", (req, res) => {
+  //   let data = { model_name: "oppo" };
+  //   let updates = { $set: { model_name: "Newoppo" } };
+  //   dbc.collection("bike_datas").updateMany(data, updates, (err, result) => {
+  //     if (err) throw err;
+  //     console.log("updated");
+  //     console.log(result);
+  //   });
+  // });
+
+  app.get('/update',(req,res)=>{
+    let data = { model_name: "oppo" };
+    let updates = { $set: { model_name: "Newoppo" } };
+    dbc.collection("bike_datas").updateOne(data,updates,(err,res)=>{
+        if(err) throw err
+        console.log(JSON.stringify("Updates done"+res))
+    })
+})
+
+  
+  
+
+  app.get("/delete", (req, res) => {
+    let data = { model_name: "vivo" };
+    dbc.collection("bike_datas").deleteOne(data, (err, res) => {
+      if (err) throw err;
+      console.log("Deleted");
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening at 3000");
+})
